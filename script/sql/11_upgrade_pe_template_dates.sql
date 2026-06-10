b800905e189a4bb06a6d78b65f00be44c6d53727
park.laetmongkol <yamchaiyaphum.laetmongkol@gmail.com>
2026-06-10 16:18:35 +0700

    delete file sql and md

--- script/sql/11_upgrade_pe_template_dates.sql
+++
@@ -1,7 +0,0 @@
--- PE: วันเริ่ม–สิ้นสุดของแบบประเมิน
-ALTER TABLE pe_evaluation_template
-  ADD COLUMN IF NOT EXISTS start_date DATE,
-  ADD COLUMN IF NOT EXISTS end_date DATE;
-
-COMMENT ON COLUMN pe_evaluation_template.start_date IS 'วันเริ่มใช้แบบประเมิน';
-COMMENT ON COLUMN pe_evaluation_template.end_date IS 'วันสิ้นสุดแบบประเมิน';
--- script/sql/12_upgrade_pe_template_period.sql
+++
@@ -1,5 +0,0 @@
--- PE: ช่วงประเมิน ครึ่งแรก / ครึ่งหลัง
-ALTER TABLE pe_evaluation_template
-  ADD COLUMN IF NOT EXISTS evaluation_period VARCHAR(10);
-
-COMMENT ON COLUMN pe_evaluation_template.evaluation_period IS 'ช่วงประเมิน: H1=ครึ่งแรก, H2=ครึ่งหลัง';
